a37682d0891ff428e6891583b0629ef3c8b5c953
j00zek <j00zek@ubuntu.(none)>
2015-10-19 13:39:40 +0200

    initial commit

---
+++ SyncWithGitLab.sh
@@ -0,0 +1,15 @@
+
+myDir=`dirname $0`
+
+if [ ! -d ~/iptvplayer-GitLab-master-version ];then
+  echo 'Cloning...'
+  git clone https://gitlab.com/iptvplayer-for-e2/iptvplayer-for-e2.git ~/iptvplayer-GitLab-master-version
+else
+  echo 'Syncing GitLab...'
+  cd ~/iptvplayer-GitLab-master-version
+  git pull
+fi
+echo 'Syncing Github...'
+cp -a ~/iptvplayer-GitLab-master-version/IPTVPlayer $myDir/
+cp -a ~/iptvplayer-GitLab-master-version/setup_translate.py $myDir/
+cp -a ~/iptvplayer-GitLab-master-version/setup.py $myDir/
---
+++ readme.txt
@@ -0,0 +1,4 @@
+This is just a mirror of https://gitlab.com/iptvplayer-for-e2/iptvplayer-for-e2.gitlab project
+I does NOT contain any changes to the original project.
+
+NOTE: All original licenses applies to it too.
